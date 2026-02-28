77bd89c683c0ba4b13e912c77e9e567fa8d70192
Luke <lukearmstrong2201@hotmail.com>
2026-02-27 17:19:16 -0800

    Add task list app.jsx and styling to task list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react";
+import { getTasks } from "./api/tasks";
+import "./styles/styles.css";
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  // Ensures only renders once
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    try {
+      const data = await getTasks();
+      setTasks(data);
+    } catch (error) {
+      console.error("Failed to fetch tasks:", error);
+    }
+  };
+
   return (
     <div className="container">
       <h1>Task Manager</h1>
+
+      {tasks.length === 0 ? (
+        <p>No tasks found.</p>
+      ) : (
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>{task.title}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
